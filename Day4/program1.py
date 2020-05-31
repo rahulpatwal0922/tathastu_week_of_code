a30902771e633060117ad6698b1009d4b6586257
rahulpatwal0922 <66076661+rahulpatwal0922@users.noreply.github.com>
2020-05-31 23:03:54 +0530

    Create program1.py

---
+++ Day4/program1.py
@@ -0,0 +1,7 @@
+from collections import Counter
+n = int(input('enter number of elements in tuple'))
+T = input('Enter elements seperated by space').split()
+
+ele = input('enter element whose occurence you want to search')
+O = Counter(T)
+print(O[ele])
