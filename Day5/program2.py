893e61f3f9549fe0ed58c592712e13fb3a18b72b
rahulpatwal0922 <66076661+rahulpatwal0922@users.noreply.github.com>
2020-06-02 01:18:33 +0530

    Create program2.py

---
+++ Day5/program2.py
@@ -0,0 +1,7 @@
+arr = list(map(int,input("Enter element seperated by space").split()))
+n = len(arr)
+for i in range(n-1):
+    great = max(arr[i+1:])
+    arr[i]=great
+arr[-1]=-1
+print("Array after replacing each element with the greatest element on the right side",arr)
